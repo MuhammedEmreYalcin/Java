a8bc93b7385cde07bfa6af3c161e7690cfa4d865
Muhammed Emre Yalcin <muhammedemreyalcin2000@live.de>
2024-02-05 14:13:54 +0300

    Create Main.java

---
+++ Introduction to Java/AbstractClasses/Main.java
@@ -0,0 +1,8 @@
+// Abstract class is a restricted class that cannot be used to create objects
+
+public class Main {
+    public static void main(String[] args){
+        WomanGameCalculator womanGameCalculator = new WomanGameCalculator();
+        womanGameCalculator.hesapla();
+    }
+}
