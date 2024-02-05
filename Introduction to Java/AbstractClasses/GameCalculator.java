3a57013ae3a9ca9901163b5558b28f63533870ab
Muhammed Emre Yalcin <muhammedemreyalcin2000@live.de>
2024-02-05 14:16:27 +0300

    Create GameCalculator.java

---
+++ Introduction to Java/AbstractClasses/GameCalculator.java
@@ -0,0 +1,9 @@
+public abstract class GameCalculator
+{
+  // hesapla is the abstract class
+    public abstract void hesapla();
+
+    public void gameover(){
+        System.out.println("Oyun bitti");
+    }
+}
