f0315efdb35babf97de0ffa69195b52c1949bc98
Muhammed Emre Yalcin <muhammedemreyalcin2000@live.de>
2024-02-05 13:49:47 +0300

    Create DortIslem.java

---
+++ Introduction to Java/MethodsOverloading/DortIslem.java
@@ -0,0 +1,10 @@
+public class DortIslem {
+    public int Topla(int sayi1, int sayi2) {
+        return sayi1 + sayi2;
+    }
+
+    // Method Overloading (having two or more methods (or functions) in a class with the same name and different arguments (or parameters))
+    public int Topla(int sayi1, int sayi2, int sayi3) {
+        return sayi1 + sayi2 + sayi3;
+    }
+}
