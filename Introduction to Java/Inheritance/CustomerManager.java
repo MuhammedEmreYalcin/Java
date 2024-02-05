dc4c10362717bb8bb9936608e4e2476ad5a1b1df
Muhammed Emre Yalcin <muhammedemreyalcin2000@live.de>
2024-02-05 14:02:03 +0300

    Create CustomerManager.java

---
+++ Introduction to Java/Inheritance/CustomerManager.java
@@ -0,0 +1,13 @@
+// CustomerManager class inherits from PersonManager Class
+
+public class CustomerManager extends PersonManager {
+    public void List()
+    {
+        System.out.println("Listelendi");
+    }
+
+    public void Add()
+    {
+        System.out.println("Eklendi");
+    }
+}
