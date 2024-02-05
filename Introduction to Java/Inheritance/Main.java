75058efb26589ef379663ac8ac6456c695c02dc2
Muhammed Emre Yalcin <muhammedemreyalcin2000@live.de>
2024-02-05 13:59:05 +0300

    Create Main.java

---
+++ Introduction to Java/Inheritance/Main.java
@@ -0,0 +1,17 @@
+/*In Java, it is possible to inherit attributes and methods from one class to another. We group the "inheritance concept" into two categories:
+
+subclass (child) - the class that inherits from another class
+superclass (parent) - the class being inherited from
+To inherit from a class, use the extends keyword.*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        Employee employee = new Employee();
+
+        EmployeeManager employeeManager = new EmployeeManager();
+        CustomerManager customerManager = new CustomerManager();
+        System.out.println(customerManager.list);
+    }
+}
