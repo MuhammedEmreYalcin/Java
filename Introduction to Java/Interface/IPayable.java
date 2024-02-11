37f76e65232750c399c1274bdcbe212d76b7cff7
Muhammed Emre Yalcin <muhammedemreyalcin2000@live.de>
2024-02-11 12:54:52 +0300

    Create IPayable.java

---
+++ Introduction to Java/Interface/IPayable.java
@@ -0,0 +1,6 @@
+// To see the interface we start the Interface name with an I. 
+// An interface is a completely "abstract class" that is used to group related methods with empty bodies:
+
+public interface IPayable {
+    void pay();
+}
