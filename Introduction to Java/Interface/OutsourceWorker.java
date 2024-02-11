9851d6181277216ca76bba8690577c26f3a68514
Muhammed Emre Yalcin <muhammedemreyalcin2000@live.de>
2024-02-11 12:50:44 +0300

    Create OutsourceWorker.java

--- Introduction to Java/Interface/OutsourceWorker.java
+++ Introduction to Java/Interface/OutsourceWorker.java
@@ -1,3 +1,4 @@
+// The OutsourceWorker class implements the interface IWorkable. The OutsourceWorker class does not implements the interfaces IEatable, IPayable because an OutsourceWorker does not get the food or wage from us. He gets it from his own company
 
 public class OutsourceWorker implements IWorkable{
 
