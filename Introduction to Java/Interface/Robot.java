ed4b5096d33504ac99bacca8c8223c7bdb5a1ade
Muhammed Emre Yalcin <muhammedemreyalcin2000@live.de>
2024-02-11 12:47:43 +0300

    Create Robot.java

---
+++ Introduction to Java/Interface/Robot.java
@@ -0,0 +1,9 @@
+// The worker class implements the interface IWorkable. The Worker class does not implements the interfaces IEatable, IPayable because an robot does not need food or wage
+
+public class Robot implements IWorkable{
+
+    @Override
+    public void work() {
+
+    }
+}
