3b837806791d071578aadaf268d6dc65a44613b4
Muhammed Emre Yalcin <muhammedemreyalcin2000@live.de>
2024-02-11 12:40:41 +0300

    Create Manin.java

---
+++ Introduction to Java/Interface/Manin.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+      // Interface = An interface is a completely "abstract class" that is used to group related methods with empty bodies:
+        // add the code you want to run
+    }
+}
